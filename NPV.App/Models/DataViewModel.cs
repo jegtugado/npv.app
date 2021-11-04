5220384eb09f07dfac9c4811582233ba8c2ea674
John Ephraim Tugado <johnt@magenic.com>
2021-11-05 02:11:25 +0800

    modify features

--- NPV.App/Models/DataViewModel.cs
+++ NPV.App/Models/DataViewModel.cs
@@ -10,10 +10,10 @@ namespace NPV.App.Models
         public decimal DiscountFactor => (1 / (decimal)Math.Pow((1 + ((double)DiscountRate / 100)), Year));
         public decimal CashFlow { get; set; }
         // PV = CF * DF
-        public decimal PresentValue => (CashFlow * DiscountFactor);
+        public decimal PresentValue => Math.Truncate(100 * (CashFlow * DiscountFactor)) / 100;
 
         //DV = CF - PV
-        public decimal DiscountedValue => CashFlow - PresentValue;
+        public decimal DiscountedValue => Math.Truncate(100 * (CashFlow - PresentValue)) / 100;
 
         public DataViewModel(int year, decimal discountRate, decimal cashFlow)
         {
