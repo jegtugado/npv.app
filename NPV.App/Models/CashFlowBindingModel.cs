590fb04abdac3260dd56b7ac1fbff1f7ca433e35
John Ephraim Tugado <johnt@magenic.com>
2021-11-05 18:04:56 +0800

    add ignored files to SC

---
+++ NPV.App/Models/CashFlowBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NPV.App.Models
+{
+    public class CashFlowBindingModel
+    {
+        public int Id { get; set; }
+        [Required]
+        [Range(1, double.MaxValue)]
+        public decimal Value { get; set; }
+    }
+}
---
+++ NPV.App/Models/DiscountTableViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace NPV.App.Models
+{
+    public class DiscountTableViewModel
+    {
+        public IEnumerable<DataViewModel> Data { get; private set; }
+        public decimal NetPresentValue => this.Data.Sum(r => r.PresentValue);
+
+        public DiscountTableViewModel(IEnumerable<DataViewModel> data)
+        {
+            this.Data = data;
+        }
+    }
+}
